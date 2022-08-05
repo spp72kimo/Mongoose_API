b6fb1f61bcf1974b5956dff4860cbd6eb6e4c47a
Steven Ke <spp72kimo@gmail.com>
2022-08-05 16:31:20 +0800

    enable cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const Comment = require("./models/comment");
 const bodyParser = require("body-parser");
-// const cors = require("cors");
+const cors = require("cors");
 const express = require("express");
 const app = new express();
 const port = 3333;
@@ -16,7 +16,7 @@ mongoose
     console.log(err);
   });
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
